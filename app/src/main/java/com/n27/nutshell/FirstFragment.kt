eaa0da9ab43558836ea3efb8709b83dd4b8b643f
Jorge <jorge.dominguez@n26.com>
2024-08-09 15:58:43 +0200

    Menu removed

--- app/src/main/java/com/n27/nutshell/FirstFragment.kt
+++ app/src/main/java/com/n27/nutshell/FirstFragment.kt
@@ -15,18 +15,16 @@ class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
