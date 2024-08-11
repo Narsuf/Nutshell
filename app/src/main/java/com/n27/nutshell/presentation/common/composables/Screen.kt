f997c06a2fa0199bebd58f001661169237ea6d03
Jorge <jorge.dominguez@n26.com>
2024-08-11 18:34:32 +0200

    Layout fixed

--- app/src/main/java/com/n27/nutshell/presentation/common/composables/Screen.kt
+++ app/src/main/java/com/n27/nutshell/presentation/common/composables/Screen.kt
@@ -1,7 +1,9 @@
 package com.n27.nutshell.presentation.common.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -10,15 +12,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun Screen(title: String, content: @Composable ColumnScope.() -> Unit) {
+fun Screen(
+    title: String,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    content: @Composable ColumnScope.() -> Unit
+) {
 
     Scaffold(
         topBar = { Toolbar(title) }
     ) { paddingValues ->
         Column(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(paddingValues)
                 .verticalScroll(rememberScrollState()),
+            verticalArrangement = verticalArrangement,
             content = content
         )
     }
--- app/src/main/java/com/n27/nutshell/presentation/detail/composables/DetailScreen.kt
+++ app/src/main/java/com/n27/nutshell/presentation/detail/composables/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.n27.nutshell.presentation.detail.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -7,11 +8,16 @@ import androidx.compose.ui.res.stringResource
 import com.n27.nutshell.R
 import com.n27.nutshell.presentation.common.composables.NavItem
 import com.n27.nutshell.presentation.common.composables.Screen
+import com.n27.nutshell.presentation.common.composables.Text
 
 @Composable
 fun DetailScreen(onClick: () -> Unit) {
 
-    Screen(title = stringResource(R.string.detail_fragment_label)) {
+    Screen(
+        title = stringResource(R.string.detail_fragment_label),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "Hola")
         BottomNavigation(backgroundColor = MaterialTheme.colorScheme.background) {
             listOf(
                 NavItem(
