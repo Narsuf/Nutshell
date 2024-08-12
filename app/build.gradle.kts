4f997eedc41786e42121dc30afa10c6fdbfa24ef
Jorge <jorge.dominguez@n26.com>
2024-08-12 21:00:59 +0200

    Version code increased

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "org.n27.nutshell"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
