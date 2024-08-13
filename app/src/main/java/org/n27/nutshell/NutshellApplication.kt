9a4d7d47e7226bd9d1df3102db9140c3f72466f0
Jorge <jorge.dominguez@n26.com>
2024-08-13 13:17:58 +0200

    .idea added to .gitignore

--- app/src/main/java/org/n27/nutshell/NutshellApplication.kt
+++ app/src/main/java/org/n27/nutshell/NutshellApplication.kt
@@ -1,6 +1,7 @@
 package org.n27.nutshell
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import org.n27.nutshell.di.AppComponent
 import org.n27.nutshell.di.AppModule
 import org.n27.nutshell.di.DaggerAppComponent
@@ -10,4 +11,9 @@ class NutshellApplication : Application() {
     val appComponent: AppComponent = DaggerAppComponent.builder()
         .appModule(AppModule(this))
         .build()
+
+    override fun onCreate() {
+        FirebaseApp.initializeApp(this)
+        super.onCreate()
+    }
 }
