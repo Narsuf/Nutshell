01edf854def597ce1e6a36a2e2069978a8182297
Jorge <jorge.dominguez@n26.com>
2024-08-18 18:09:55 +0200

    ErrorMapperKtTest created

---
+++ app/src/test/java/org/n27/nutshell/presentation/common/mapping/ErrorMapperKtTest.kt
@@ -0,0 +1,31 @@
+package org.n27.nutshell.presentation.common.mapping
+
+import com.n27.nutshell.presentation.getError
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.n27.nutshell.Constants.NO_INTERNET_CONNECTION
+import org.n27.nutshell.R
+
+class ErrorMapperKtTest {
+
+    @Test
+    fun `should return expected generic error`() {
+        val expected = getError()
+
+        val actual = Throwable().toError()
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return expected no internet error`() {
+        val expected = getError(
+            R.string.no_internet_error_title,
+            R.string.no_internet_error_description
+        )
+
+        val actual = Throwable(NO_INTERNET_CONNECTION).toError()
+
+        assertEquals(expected, actual)
+    }
+}
--- test_data/src/main/java/com/n27/nutshell/presentation/ErrorDataGenerator.kt
+++ test_data/src/main/java/com/n27/nutshell/presentation/ErrorDataGenerator.kt
@@ -1,8 +1,12 @@
 package com.n27.nutshell.presentation
 
+import androidx.annotation.StringRes
 import org.n27.nutshell.presentation.common.model.Error
 
-fun getError() = Error(
-    title = org.n27.nutshell.R.string.generic_error_title,
-    description = org.n27.nutshell.R.string.generic_error_description
+fun getError(
+    @StringRes title: Int = org.n27.nutshell.R.string.generic_error_title,
+    @StringRes description: Int = org.n27.nutshell.R.string.generic_error_description
+) = Error(
+    title = title,
+    description = description
 )
